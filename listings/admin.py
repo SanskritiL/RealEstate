1a92332e43c5ed6e7adfab81d079daa5029b347b
Sanskriti Lamsal <sanskritilamsal@hst000149268um.wifi02.olemiss.edu>
2019-01-16 17:02:16 -0600

    The styling has been taken care of & the admin page has been created

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+
+from .models import Listing
 # Register your models here.
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title','is_published','price','list_date','realtor')
+    list_display_links=('id','title')
+    list_filter = ('realtor',)
+    list_editable=('is_published',)
+
+admin.site.register(Listing, ListingAdmin)
