42f868d7cb994958e468880fa77609d7e742d70c
Sanskriti Lamsal <sanskritilamsal@hst000149268um.wifi02.olemiss.edu>
2019-01-17 11:09:01 -0600

    listings are dynamically added

--- listings/admin.py
+++ listings/admin.py
@@ -9,5 +9,8 @@ class ListingAdmin(admin.ModelAdmin):
     list_display_links=('id','title')
     list_filter = ('realtor',)
     list_editable=('is_published',)
+    search_fields=('title','description','address','city','state','zipcode','price')
+    list_per_page =25
+
 
 admin.site.register(Listing, ListingAdmin)
